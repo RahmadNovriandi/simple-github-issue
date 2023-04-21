940d365ccf0ca19d0acf42fdb3c260acbd74ee5a
RahmadNovriandi <128124546+RahmadNovriandi@users.noreply.github.com>
2023-04-22 00:49:15 +0700

    profil
    profil

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SimpleConfig(AppConfig):
+class ProfilConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'simple'
+    name = 'profil'
